899a8f6a179af3073ca983b0479dd8d21178f63e
Sam <samyankun@gmail.com>
2018-10-09 15:54:05 +0800

    remove the non-essential key

--- src/renderer/store/modules/Counter.js
+++ src/renderer/store/modules/Counter.js
@@ -3,7 +3,7 @@ import MongoClient from 'mongodb'
 var config = {
   chainId: '8a679bd6c011ff93eff0cb99d997bf72a69a0aaddc430d7e9c4b705f4de4d843',
   httpEndpoint: 'http://127.0.0.1:8888',
-  keyProvider: ['5KiDMuzQcK8q3M2MKHLyY4azLq8WnyYHdRN9VCsBh5sq6gDXnuf', '5Hz2G2L9p3k7YhkqGJaioNJQnYKjtZKKS2y3wRLDobCQAtXg5oA', '5KVEXrFj2AWvBzo9UkiZnw1A6k3vEfcFp99R2XdM2PdWk3spjEm', '5J1sBkezsqpwUeFL1e54gwWgqBzmy3SHxYmmDq57ecjL4zjzkSU', '5KJ6ygSpfjfTqQ58bzqwGTJN7wA8y5Rgy8WFkZDub4ZxcBf7ZAW', '5JUzVys5jc8pBiAQQG1QRcZVMFd8CyHAU3LTkn83qQw7HksniiL'],
+  keyProvider: ['5KiDMuzQcK8q3M2MKHLyY4azLq8WnyYHdRN9VCsBh5sq6gDXnuf', '5Hz2G2L9p3k7YhkqGJaioNJQnYKjtZKKS2y3wRLDobCQAtXg5oA'],
   expireInSeconds: 60,
   broadcast: true,
   sign: true
